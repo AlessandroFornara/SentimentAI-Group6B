fd17d6b11ee6768661ff2df5106d22f47b3fb2b4
Alessandro Fornara <alessandro1.fornara@mail.polimi.it>
2024-11-19 19:03:21 +0100

    Project setup

---
+++ src/main/java/polimi/aui/sentimentaigroup6b/SentimentAiGroup6BApplication.java
@@ -0,0 +1,13 @@
+package polimi.aui.sentimentaigroup6b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentAiGroup6BApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentimentAiGroup6BApplication.class, args);
+    }
+
+}
---
+++ src/test/java/polimi/aui/sentimentaigroup6b/SentimentAiGroup6BApplicationTests.java
@@ -0,0 +1,13 @@
+package polimi.aui.sentimentaigroup6b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentimentAiGroup6BApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
