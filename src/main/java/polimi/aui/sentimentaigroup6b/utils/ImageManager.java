bea50499e13372257dac26c21beb85c4af93f207
Alessandro Fornara <alessandro1.fornara@mail.polimi.it>
2025-01-22 12:05:16 +0100

    Fixed ImageManager to make it work with jar

--- src/main/java/polimi/aui/sentimentaigroup6b/utils/ImageManager.java
+++ src/main/java/polimi/aui/sentimentaigroup6b/utils/ImageManager.java
@@ -5,40 +5,57 @@ import org.springframework.stereotype.Component;
 import polimi.aui.sentimentaigroup6b.models.ImageResponse;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.net.URL;
+import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.Base64;
+import java.util.Collections;
 import java.util.List;
 
 @Component
 public class ImageManager {
 
     public List<ImageResponse> extractImages() {
-
         List<ImageResponse> images = new ArrayList<>();
 
         try {
-            ClassPathResource resource = new ClassPathResource("static/images");
-            Path imagesPath = resource.getFile().toPath();
+            ClassLoader classLoader = getClass().getClassLoader();
+            URL resourceUrl = classLoader.getResource("static/images");
 
-            Files.list(imagesPath).forEach(filePath -> {
-                try {
-                    byte[] imageBytes = Files.readAllBytes(filePath);
-                    String contentType = Files.probeContentType(filePath);
+            if (resourceUrl == null) {
+                System.out.println("Directory 'static/images' not found");
+                return images;
+            }
 
-                    String base64Data = Base64.getEncoder().encodeToString(imageBytes);
-
-                    images.add(new ImageResponse(filePath.getFileName().toString(), contentType, base64Data));
-                } catch (IOException e) {
-                    System.out.println("Error reading image: " + filePath.getFileName());
+            if (resourceUrl.getProtocol().equals("jar")) {
+                try (FileSystem fileSystem = FileSystems.newFileSystem(resourceUrl.toURI(), Collections.emptyMap())) {
+                    Path imagesPath = fileSystem.getPath("static/images");
+                    processDirectory(imagesPath, images);
                 }
-            });
+            } else {
+                Path imagesPath = Paths.get(resourceUrl.toURI());
+                processDirectory(imagesPath, images);
+            }
 
-            return images;
-
-        } catch (IOException e) {
+        } catch (Exception e) {
             return null;
         }
+
+        return images;
+    }
+
+    private void processDirectory(Path directoryPath, List<ImageResponse> images) throws IOException {
+        Files.walk(directoryPath).filter(Files::isRegularFile).forEach(filePath -> {
+            try {
+                byte[] imageBytes = Files.readAllBytes(filePath);
+                String contentType = Files.probeContentType(filePath);
+
+                String base64Data = Base64.getEncoder().encodeToString(imageBytes);
+
+                images.add(new ImageResponse(filePath.getFileName().toString(), contentType, base64Data));
+            } catch (IOException e) {
+                System.out.println("Error reading image: " + filePath.getFileName());
+            }
+        });
     }
 }
